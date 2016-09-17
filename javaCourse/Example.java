0e2592655d22c4f484c4580072bbcc4b433a24b7
UmakantKulkarni <kulkarni.u@husky.neu.edu>
2016-09-17 18:43:34 -0400

    javacourse

---
+++ javaCourse/Example.java
@@ -0,0 +1,13 @@
+/*
+This is an Example Program
+*/
+
+class Example {
+	
+	// JAVA Program starts here with a call to main ()
+	public static void main (String args[]) {
+		System.out.println("Hello world");
+	}
+
+
+}
